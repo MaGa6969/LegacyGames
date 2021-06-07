cede4fcd26e1b4ec7c4a300ac495fda599a5040f
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-06-07 12:25:13 +0200

    creando el constructor de game.java

--- src/main/java/org/factoriaf5/legacygames/models/Games.java
+++ src/main/java/org/factoriaf5/legacygames/models/Games.java
@@ -1,4 +1,39 @@
 package org.factoriaf5.legacygames.models;
 
 public class Games {
+    private Long id;
+    private String title;
+    private Character platform;
+    private int year;
+    private Double price;
+    private String etiqueta;
+    private int discount;
+    private Double priceDiscount;
+    private String category;
+    private String publisher;
+    private int pegi;
+    private String pegiDescriptors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Character getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Character platform) {
+        this.platform = platform;
+    }
 }
