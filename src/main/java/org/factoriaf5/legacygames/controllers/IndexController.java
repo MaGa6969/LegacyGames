ce61e650d9cc2c2fd9be665c59a866c8fd83fe19
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-06-08 10:12:19 +0200

    base de datos funciona

--- src/main/java/org/factoriaf5/legacygames/controllers/IndexController.java
+++ src/main/java/org/factoriaf5/legacygames/controllers/IndexController.java
@@ -11,18 +11,8 @@ import java.util.List;
 @Controller
 public class IndexController {
 
-    private GameService gameService;
-
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @GetMapping("/games")
-    String listGames(Model model) {
-        List<Game> games = gameService.allGames();
-        model.addAttribute("title", "Book list");
-        model.addAttribute("books", games);
-        return "books/all";
     }
 }
