9c72fc647b734e7b814bebb6c43fcf477e80741c
superco-art <daniela.trifu@gmail.com>
2021-06-08 15:17:40 +0200

    data sql lista

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (0, 'Theft Auto: San Andreas', 'PS2', '2004', '24.99', 'SUPER', '10', '22.49', 'Action', 'Take-Two Interactive', '18', 'extreme violence');
+VALUES (1, 'Theft Auto: San Andreas', 'PS2', '2004', '24.99', 'SUPER', '10', '22.49', 'Action', 'Take-Two Interactive', '18', 'extreme violence');
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (1, 'Wii Sports', 'Wii', '2006', '19.99', '', '', '', 'Sports', 'Nintendo', '7', '');
+VALUES (2, 'Wii Sports', 'Wii', '2006', '19.99', 'NULL', '0', '0', 'Sports', 'Nintendo', '7', 'NULL');
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (2, 'Super Mario Bros.', 'NES', '1985', '14.99', '', '', '', 'Platform', 'Nintendo', '7', '');
+VALUES (3, 'Super Mario Bros.', 'NES', '1985', '14.99', 'NULL', '0', '0', 'Platform', 'Nintendo', '7', 'NULL');
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (3, 'Mario Kart Wii', 'Wii', '2008', '9.99', '', '', '', 'Racing', 'Nintendo', '7', '');
+VALUES (4, 'Mario Kart Wii', 'Wii', '2008', '9.99', 'NULL', '0', '0', 'Racing', 'Nintendo', '7', 'NULL');
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (4, 'Wii Sports Resort', 'Wii', '2009', '9.99', '', '', '', 'Sports', 'Nintendo', '7', '');
+VALUES (5, 'Wii Sports Resort', 'Wii', '2009', '9.99', 'NULL', '0', '0', 'Sports', 'Nintendo', '7', 'NULL');
 INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
-VALUES (5, 'Pokemon Red/Pokemon Blue', 'GB', '1996', '', '', '', '', 'Role-Playing', 'Nintendo', '7', '');
+VALUES (6, 'Pokemon Red/Pokemon Blue', 'GB', '1996', '0', 'NULL', '0', '0', 'Role-Playing', 'Nintendo', '7', 'NULL');
 
 
