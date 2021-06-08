4390cdedb4d7cb93634ac5a44cc97b95b9781581
superco-art <daniela.trifu@gmail.com>
2021-06-08 14:56:22 +0200

    tabla game

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (0, 'Theft Auto: San Andreas', 'PS2', '2004', '24.99', 'SUPER', '10', '22.49', 'Action', 'Take-Two Interactive', '18', 'extreme violence');
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (1, 'Wii Sports', 'Wii', '2006', '19.99', '', '', '', 'Sports', 'Nintendo', '7', '');
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (2, 'Super Mario Bros.', 'NES', '1985', '14.99', '', '', '', 'Platform', 'Nintendo', '7', '');
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (3, 'Mario Kart Wii', 'Wii', '2008', '9.99', '', '', '', 'Racing', 'Nintendo', '7', '');
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (4, 'Wii Sports Resort', 'Wii', '2009', '9.99', '', '', '', 'Sports', 'Nintendo', '7', '');
+INSERT INTO game(id, title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
+VALUES (5, 'Pokemon Red/Pokemon Blue', 'GB', '1996', '', '', '', '', 'Role-Playing', 'Nintendo', '7', '');
+
+
