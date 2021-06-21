7d7068423d76f531f9b7c0b75e880887c7265d5a
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-06-21 09:24:06 +0200

    añadido punto y coma

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO game(title, platform, year, price, etiqueta, discount, price_discount, category, publisher, pegi, pegi_descriptors)
 VALUES ('Theft Auto: San Andreas', 'PS2', '2004', '24.99', 'SUPER', '10', '22.49', 'Action', 'Take-Two Interactive', '18', 'extreme violence'),
-        ('Super Mario Bros.', 'NES', '1985', '14.99', 'NULL', '0', '0', 'Platform', 'Nintendo', '7', 'NULL')
+        ('Super Mario Bros.', 'NES', '1985', '14.99', 'NULL', '0', '0', 'Platform', 'Nintendo', '7', 'NULL');
 
 
 
