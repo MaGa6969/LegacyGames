7a40a2e837e9a8a6bb9d9417dedbb3c6420cfb58
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-06-30 14:51:49 +0200

    filtrando por categorias

--- src/main/java/org/factoriaf5/legacygames/services/CategoryService.java
+++ src/main/java/org/factoriaf5/legacygames/services/CategoryService.java
@@ -2,6 +2,7 @@ package org.factoriaf5.legacygames.services;
 
 import org.factoriaf5.legacygames.models.Category;
 import org.factoriaf5.legacygames.models.CategoryRepository;
+import org.factoriaf5.legacygames.models.Game;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,5 +17,9 @@ public class CategoryService {
 
     public List<Category> allCategories() {
         return (List<Category>) categoryRepository.findAll();
+    }
+
+    public Category getCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId).get();
     }
 }
